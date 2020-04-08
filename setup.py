dc7ec27b04750cced0dfe17c63f3d42a26ee014c
Abishalini <abi.gpuram@gmail.com>
2020-04-08 06:55:48 -0500

    Add contents to init.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='packaging_test',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/Abishalini/packaging_test.git',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['packaging_test'],
+      zip_safe=False)
